0fc9daf967e8bd3f198f0c8df0bacc2b6b62cee5
Yere4848 <yeremiayuliyanto9@gmail.com>
2021-09-07 08:58:34 +0700

    layout + contoh dashboard
    layout + contoh dashboard sb admin 2

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,99 @@
+ <!-- Sidebar -->
+ <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+     <!-- Sidebar - Brand -->
+     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+         <div class="sidebar-brand-icon rotate-n-15">
+             <i class="fas fa-laugh-wink"></i>
+         </div>
+         <div class="sidebar-brand-text mx-3">Store</div>
+     </a>
+
+     <!-- Divider -->
+     <hr class="sidebar-divider my-0">
+
+     <!-- Nav Item - Dashboard -->
+     <li class="nav-item active">
+         <a class="nav-link" href="index.html">
+             <i class="fas fa-fw fa-tachometer-alt"></i>
+             <span>Dashboard</span></a>
+     </li>
+
+     <!-- Divider -->
+     <hr class="sidebar-divider">
+
+     <!-- Heading -->
+     <div class="sidebar-heading">
+         Master
+     </div>
+
+     <!-- Nav Item - Pages Collapse Menu -->
+     <li class="nav-item">
+         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
+             aria-controls="collapseTwo">
+             <i class="fas fa-database"></i>
+             <span>Master Data</span>
+         </a>
+         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+             <div class="bg-white py-2 collapse-inner rounded">
+                 <a class="collapse-item" href="buttons.html">Inventory</a>
+                 <a class="collapse-item" href="cards.html">Service</a>
+             </div>
+         </div>
+     </li>
+
+     <hr class="sidebar-divider">
+
+     <!-- Heading -->
+     <div class="sidebar-heading">
+         Reports
+     </div>
+
+     <!-- Nav Item - Pages Collapse Menu -->
+     <li class="nav-item">
+         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true"
+             aria-controls="collapseTwo">
+             <i class="fas fa-file"></i>
+             <span>Report</span>
+         </a>
+         <div id="collapseThree" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+             <div class="bg-white py-2 collapse-inner rounded">
+                 <a class="collapse-item" href="buttons.html">Report Daily</a>
+                 <a class="collapse-item" href="buttons.html">Report Weekly</a>
+                 <a class="collapse-item" href="cards.html">Report Monthly</a>
+             </div>
+         </div>
+     </li>
+
+     <hr class="sidebar-divider">
+
+     <!-- Heading -->
+     <div class="sidebar-heading">
+         Transactions
+     </div>
+
+     <!-- Nav Item - Pages Collapse Menu -->
+     <li class="nav-item">
+         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true"
+             aria-controls="collapseTwo">
+             <i class="fas fa-shopping-cart"></i>
+             <span>Transaction</span>
+         </a>
+         <div id="collapseFour" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+             <div class="bg-white py-2 collapse-inner rounded">
+                 <a class="collapse-item" href="buttons.html">Incoming Transaction</a>
+                 <a class="collapse-item" href="cards.html">Outgoing Transaction</a>
+             </div>
+         </div>
+     </li>
+
+     <!-- Divider -->
+     <hr class="sidebar-divider d-none d-md-block">
+
+     <!-- Sidebar Toggler (Sidebar) -->
+     <div class="text-center d-none d-md-inline">
+         <button class="rounded-circle border-0" id="sidebarToggle"></button>
+     </div>
+
+ </ul>
+ <!-- End of Sidebar -->
